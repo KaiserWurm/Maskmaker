db8176437936b60671aedda12826c1bdbf4458f0
KaiserWurm <kai.bradford@gmail.com>
2026-02-01 12:52:48 -0700

    Added scene Transitions

--- maskmaker-(copy)/test.gd
+++ maskmaker-(copy)/test.gd
@@ -6,6 +6,9 @@ extends Node2D
 @onready var maskselector =$"Mask Selection"
 @export var talkheight = 10
 var rest
+var endingbegun = false
+@export_file("*.tscn") var nextscene: String
+@export var discovery: AudioStream
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,19 +17,24 @@ func _ready() -> void:
 	DialogueManager.show_dialogue_balloon(words, "start")
 	rest = $TempMan.position.y
 
-func plothappens():
-	$BackgroundMusic.stop()
-	$BackgroundMusic.stream = "res://GGJ Maskmaker Game Jam/Music/Discovery.wav"
-	$BackgroundMusic.play()
-
 func _process(delta: float) -> void:
 	if DialogueManager.speak == 1:
 		$TempMan.position.y = rest+talkheight
 	else:
 		$TempMan.position.y = rest
-
+	if DialogueManager.endingchosen != "none" && endingbegun == false:
+		endingbegun = true
+		$Ending.visible = true
+		$BackgroundMusic.stop()
+		$BackgroundMusic.stream = discovery
+		$BackgroundMusic.play()
+		
 
 
 func _on_button_button_down() -> void:
 	$"Mask Selection".visible =  true
 	$Button.visible = false
+
+
+func _on_ending_button_down() -> void:
+	get_tree().change_scene_to_file(nextscene)
